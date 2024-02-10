52d9b9aa63b62a33806f9792f9ee60096710e762
khader12345 <147363653+khader12345@users.noreply.github.com>
2024-02-10 12:21:01 -0500

    Private Method Created

--- PriceCalculatorApp/MainPage.xaml.cs
+++ PriceCalculatorApp/MainPage.xaml.cs
@@ -3,3 +3,24 @@ using System;
 using Microsoft.Maui.Controls;
 
 namespace PriceCalculatorApp
+{
+
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            discountPicker.SelectedIndex = 0;
+
+        }
+
+        private void CalculateClick (object sender, EventArgs e)
+        {
+            try
+            {
+                
+            }
+        }
+    }
+}
