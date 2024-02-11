f4a590c3b917d230fc08cf6a3ab92f9b8d43113e
khader12345 <147363653+khader12345@users.noreply.github.com>
2024-02-10 20:12:03 -0500

    Calculate Click button class

--- PriceCalculatorApp/BusinessLogic/Bill.cs
+++ PriceCalculatorApp/BusinessLogic/Bill.cs
@@ -6,31 +6,30 @@
         public double PricePerItem { get; set; }
         public double Discount { get; set; }
         public int NumberOfItems { get; set; }
-        public bool TotalProperty { get; set; } 
+        public bool PlusDiscount { get; set; } 
 
 
-        public double TotalCalculation
+        public double TotalCalculation()
         {
-            get
-            {
+            
                 double total = PricePerItem * NumberOfItems;
                 total += total * DefaultTax;
                 
-                if (TotalProperty)
+                if (PlusDiscount)
                 {
                     total -= total * (Discount / 100.0);
 
                 }
                 return total;
-            }
+            
         }
         
-        public Bill(double priceItem, double discount, int numberofitems, bool totalproperty)
+        public Bill(double priceItem, double discount, int numberofitems, bool plusDiscount)
         {
             PricePerItem = priceItem >= 0 ? priceItem :  throw new ArgumentOutOfRangeException(nameof(priceItem));
             Discount = discount;
             NumberOfItems = numberofitems >= 0 ? numberofitems : throw new ArgumentOutOfRangeException(nameof(numberofitems));
-            TotalProperty = totalproperty; 
+            PlusDiscount = plusDiscount; 
 
         }
     
--- PriceCalculatorApp/MainPage.xaml.cs
+++ PriceCalculatorApp/MainPage.xaml.cs
@@ -12,18 +12,33 @@ namespace PriceCalculatorApp
             InitializeComponent();
             discountPicker.SelectedIndex = 0;
         }
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                });
 
-#if DEBUG
-            builder.Logging.AddDebug();
-#endif
-            return builder.Build();
+        private void CalculateClick(object sender, EventArgs e)
+        {
+            try
+            {
+                double PricePerItem = double.Parse(pricePerItemEntry.Text);
+                int NumberOfItems = int.Parse(numberItemsEntry.Text);
+                double Discount = double.Parse(discountPicker.SelectedItem.ToString());
+                bool TotalProperty = plusDiscountCheckbox.IsChecked;
 
+                var bill = new Bill(PricePerItem, NumberOfItems, Discount, TotalProperty);
+                double total = bill.TotalCalculation();
+
+                resultLabel.Text = $"Total: {total:C2}";
+            }
+            catch (FormatException)
+            {
+                resultLabel.Text = "Please enter valid numbers for price and items.";
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                resultLabel.Text = $"Invalid input: {ex.ParamName}";
+            }
+
+        }
+                
+                
         }
 
 
